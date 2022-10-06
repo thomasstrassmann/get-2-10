f12447673568996d14e5d358d8cf19d848be1c8c
thomasstrassmann <thomas_strassmann@freenet.de>
2022-10-06 09:41:31 +0000

    add question catalog and two example questions. create pickQuestion and displayQuestion functions

---
+++ assets/js/catalog.js
@@ -0,0 +1,8 @@
+export const catalog = [
+    {question: "The highest mountain in South America is the... ",
+    answers: ["Aconcagua", "Ojos del Salado", "Monte Pissis"],
+    correct: 0},
+    {question: "Tsutomu Yamaguchi became famous for what?",
+    answers: ["He was the first climber of Mount Fuji", "He survived two atomic bomb drops", "Is the oldest documented man in the world "],
+    correct: 1}
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
+// Import of the catalog
+import {catalog} from './catalog.js';
+
+// Basic assignments
+
 let startButton = document.getElementById("play");
 let instructions = document.getElementById('instructions');
 let gamearea = document.getElementById('gamearea');
+
+// Event listeners
 startButton.addEventListener('click', start);
 
 
@@ -19,4 +26,24 @@ instructions.addEventListener("load", function() {
 function start(){
     instructions.style.display = "none";
     gamearea.style.display = "block";
+    pickQuestion();
 }
+
+function pickQuestion(){
+    let questionIndex = Math.floor(Math.random() * catalog.length);
+    return displayQuestion(questionIndex);
+}
+
+function displayQuestion(index){
+    let question = document.getElementById('question');
+    let answer1 = document.getElementById('answer1');
+    let answer2 = document.getElementById('answer2');
+    let answer3 = document.getElementById('answer3');
+
+    question.innerHTML = catalog[index].question;
+    answer1.innerHTML = catalog[index].answers[0];
+    answer2.innerHTML = catalog[index].answers[1];
+    answer3.innerHTML = catalog[index].answers[2];
+
+}
+
