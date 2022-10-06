dae5f70e1ddafa519d9d57717b299e700d089674
thomasstrassmann <thomas_strassmann@freenet.de>
2022-10-06 08:32:43 +0000

    make changes to the script to make transitions to gameare work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
 let startButton = document.getElementById("play");
 let instructions = document.getElementById('instructions');
 let gamearea = document.getElementById('gamearea');
+startButton.addEventListener('click', start);
 
-startButton.addEventListener('click', start());
+
+/** 
+ * Eventlistener for page reload
+ * It gets the user back to the start page / instructions
+ * resets the score
+ */
+instructions.addEventListener("load", function() {
+    instructions.style.display = "block";
+    gamearea.style.display = "none";
+    // setting the score back
+})
+
 
 function start(){
     instructions.style.display = "none";
