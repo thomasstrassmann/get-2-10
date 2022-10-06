45c69bf62549828ccfa628396b6a7e12a3e45eee
thomasstrassmann <thomas_strassmann@freenet.de>
2022-10-06 13:02:20 +0000

    add a for loop which puts a click event on all answers. they get captured by the capturAnswer function which gives this value to the checkAnswer function, which checks the value with the catalog. works fine for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,29 @@
 // Import of the catalog
 import {catalog} from './catalog.js';
 
-// Basic assignments
+// Basic assignments from DOM Elements to variables
 
-let startButton = document.getElementById("play");
+let startButton = document.getElementById('play');
 let instructions = document.getElementById('instructions');
+
 let gamearea = document.getElementById('gamearea');
+let question = document.getElementById('question');
+let answer1 = document.getElementById('answer1');
+let answer2 = document.getElementById('answer2');
+let answer3 = document.getElementById('answer3');
 
 // Event listeners
 startButton.addEventListener('click', start);
+
+// this code gives all answers an EventListener
+const givenAnswer = document.getElementsByClassName('answerGiven');
+for (let answer of givenAnswer){
+    answer.addEventListener('click', captureAnswer);
+}
+
+
+// global variable
+var special = false;
 
 
 /** 
@@ -35,15 +50,25 @@ function pickQuestion(){
 }
 
 function displayQuestion(index){
-    let question = document.getElementById('question');
-    let answer1 = document.getElementById('answer1');
-    let answer2 = document.getElementById('answer2');
-    let answer3 = document.getElementById('answer3');
-
     question.innerHTML = catalog[index].question;
     answer1.innerHTML = catalog[index].answers[0];
     answer2.innerHTML = catalog[index].answers[1];
     answer3.innerHTML = catalog[index].answers[2];
+    question.setAttribute('data-index',index);
+}
 
+function captureAnswer(e){
+    let finalAnswer = e.target.getAttribute('data-type');
+    checkAnswer(finalAnswer);
+}
+
+
+function checkAnswer(answerGiven){
+    let currentQuestionIndex = question.getAttribute("data-index");
+    if(special === false){
+        answerGiven == catalog[currentQuestionIndex].correct ? incrementScore() : decrementScore();
+    } else {
+        answerGiven == catalog[currentQuestionIndex].correct ? doubleScore() : deleteScore();
+    }
 }
 
